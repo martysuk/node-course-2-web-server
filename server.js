9bcc901710fd6e35bc239dc06c8f79f0c8f89399
marta <martysuk99@gmail.com>
2018-10-08 01:03:34 +0300

    Projects` page added

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ hbs.registerHelper('screamIt', (text) =>  text.toUpperCase())
 
 app.set('view engine', 'hbs')
 app.use((req, res, next) => {
-    
     let now = new Date().toString();
     let log = `${now}: ${req.method} ${req.url}`
     console.log(log)
@@ -54,6 +53,13 @@ app.get('/', (req, res) => {
 app.get('/about', (req,res) => {
     res.render('about.hbs', {
         pageTitle: 'About page' 
+    })
+});
+
+
+app.get('/projects', (req,res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Page about projects ' 
     })
 });
 
